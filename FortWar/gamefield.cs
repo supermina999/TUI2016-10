bb92e8a4a3f640dfb31baa541529e4312d230e0d
Skyfall25 <anton-svet25@mail.ru>
2016-06-23 16:16:51 +0300

    0.1

---
+++ FortWar/gamefield.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FortWar
+{
+    class gamefield
+    {
+        hexagon[,] hexarray;
+        public void buildfield(int borderx, int bordery, int numberofsteps, Canvas MainCanvas)
+        {
+            hexarray = new hexagon[borderx, bordery];
+            int x = 0, y = 0;
+            if (bordery >= 13 && bordery < 24)
+            {
+
+                for (int i = 0; i < borderx; i++)
+                {
+                    if ((i + 1) % 2 == 0)
+                    {
+                        y += 22;
+                    }
+                    for (int j = 0; j < bordery; j++)
+                    {
+                        hexarray[i, j] = new hexagon();
+                        hexarray[i, j].hex(x, y, 75, 44, MainCanvas);
+                        y += 43;
+                    }
+                    x += 37;
+                    y = 0;
+                }
+            }
+            else if (bordery >= 24)
+            {
+                for (int i = 0; i < borderx; i++)
+                {
+                    if ((i + 1) % 2 == 0)
+                    {
+                        y += 16;
+                    }
+                    for (int j = 0; j < bordery; j++)
+                    {
+                        hexarray[i, j] = new hexagon();
+                        hexarray[i, j].hex(x, y, 56, 32, MainCanvas);
+                        y += 32;
+                    }
+                    x += 27;
+                    y = 0;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < borderx; i++)
+                {
+                    if ((i + 1) % 2 == 0)
+                    {
+                        y += 32;
+                    }
+                    for (int j = 0; j < bordery; j++)
+                    {
+                        hexarray[i, j] = new hexagon();
+                        hexarray[i, j].hex(x, y, 111, 64, MainCanvas);
+                        y += 64;
+                    }
+                    x += 54;
+                    y = 0;
+                }
+            }
+        }
+    }
+}
---
+++ FortWar/hexagon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace FortWar
+{
+    class hexagon
+    {
+        Image geksimage;
+        public void hex(int x, int y, int w, int h, Canvas MainCanvas)
+        {
+            geksimage = new Image();
+            BitmapImage GeksImageSource = new BitmapImage();
+            GeksImageSource.BeginInit();
+            GeksImageSource.UriSource = new Uri("Geks.png", UriKind.Relative);
+            GeksImageSource.EndInit();
+            geksimage.Source = GeksImageSource;
+            Thickness GeksMarginCord = new Thickness();
+            GeksMarginCord.Left = x;
+            GeksMarginCord.Top = y;
+            geksimage.Margin = GeksMarginCord;
+            geksimage.Height = h;
+            geksimage.Width = w;
+            MainCanvas.Children.Add(geksimage);
+        }
+    }
+}
